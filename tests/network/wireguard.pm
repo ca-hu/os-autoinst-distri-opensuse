59f26fe78c0f6a81b152ff7232ca3f28f5f95931
Cathy Hu <cathy.hu@suse.com>
2024-10-31 16:09:44 +0100

    test

--- tests/network/wireguard.pm
+++ tests/network/wireguard.pm
@@ -32,7 +32,6 @@ sub start_wgquick {
 }
 
 sub run {
-    assert_script_run 'getenforce';
     assert_script_run 'semodule -DB';
     if (get_var('IS_MM_SERVER')) {
         barrier_create 'SETUP_DONE', 2;
